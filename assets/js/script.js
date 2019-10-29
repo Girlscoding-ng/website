7df07638311805d3a4322dc36e4399b4642303a1
Nelson Chinedu <egwuchinedu69@gmail.com>
2019-10-29 14:21:01 -0600

    add script to file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
+const firstname = document.querySelector("#firstname");
+const lastname = document.querySelector("#lastname");
+const emailAddress = document.querySelector("#emailAddress");
+const phoneNumber = document.querySelector("#phoneNumber");
+const message = document.querySelector("#message");
+const contactHandler = document.querySelector('#contact_handler');
+const alert = document.querySelector("#alert");
+const responseTag = document.createElement('p');
+let responseText;
+
+AOS.init();
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
   }
@@ -125,4 +137,84 @@ function openNav() {
     $this.countTo(options);
     }
   });
-  AOS.init();
+
+  
+  const isInputNumber = (evt) => {
+    let char = String.fromCharCode(evt.which);
+    if (!(/[0-9]/.test(char))) {
+      evt.preventDefault();
+    }
+  }
+
+  const errorHandler = () => {
+    if (firstname.value){
+      alert.style.display = 'none';
+      responseText.textContent = '';
+    }
+    if (lastname.value){
+      alert.style.display = 'none';
+      responseText.textContent = '';
+    }
+    if (emailAddress.value){
+      alert.style.display = 'none';
+      responseText.textContent = '';
+    }
+    if (phoneNumber.value){
+      alert.style.display = 'none';
+      responseText.textContent = '';
+    }
+    if (message.value){
+      alert.style.display = 'none';
+      responseText.textContent = '';
+    }
+  }
+
+  const sendData =  async () => {
+
+    await fetch('https:girlscoding-api.herokuapp.com/contact', {
+      credential: "same-origin",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstname: firstname.value,
+        lastname: lastname.value,
+        emailAddress: emailAddress.value,
+        phoneNumber: phoneNumber.value,
+        message: message.value
+      }),
+    })
+    .then(res => res.json())
+    .then(data => {
+      const { status, response } = data;
+      responseText = document.createTextNode(response);
+      
+      if (status === 400){
+        alert.style.display = 'block';
+        alert.setAttribute('class', 'alert alert-danger');
+        responseTag.appendChild(responseText);
+        alert.appendChild(responseTag);
+      }else if (status === 201){
+        alert.style.display = 'block';
+        alert.setAttribute('class', 'alert alert-success');
+        responseTag.appendChild(responseText);
+        alert.appendChild(responseTag);
+        firstname.value = '';
+        lastname.value = '';
+        phoneNumber.value = '';
+        emailAddress.value = '';
+        message.value = '';
+        setInterval(function(){
+          alert.style.display = 'none';
+          responseText.textContent = '';
+        }, 5000);
+      }
+    })
+  
+  }
+  
+  contactHandler.addEventListener('submit', (e) => {
+    e.preventDefault();
+    sendData();
+  })
