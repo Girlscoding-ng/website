b67516719982e2f872dd09b92c7742adb6edb163
Nelson Chinedu <egwuchinedu69@gmail.com>
2019-11-12 16:25:37 -0600

    add sw to js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 if('serviceWorker' in navigator){
     window.addEventListener('load', () => {
         navigator.serviceWorker
-          .register('../sw.js')
+          .register('./sw.js')
           .then(reg => console.log('Service worker registered'))
           .catch(err => err)
     })
